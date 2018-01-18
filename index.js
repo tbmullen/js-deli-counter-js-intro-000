d1362ad80db22def8ed2f374a70a86e2fcb5d0ab
Trevor Mullen <tbhackworth@gmail.com>
2018-01-18 22:41:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, custName) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return katzDeliLine[0];
+    return "Currently serving " + katzDeliLine[0] + ".";
     katzDeliLine.shift();
   } else {
     return "There is nobody waiting to be served!";
