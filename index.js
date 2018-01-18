6dab6a1a0be82834d0a55886685c8daf22469301
Trevor Mullen <tbhackworth@gmail.com>
2018-01-18 22:41:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(katzDeliLine, custName) {
   katzDeliLine.push(custName);
   return "Welcome, " + custName + ". You are number " + katzDeliLine.length + " in line.";
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine[0];
+    katzDeliLine.shift();
+  } else {
+    return "There is nobody waiting to be served!";
+  }
+}
