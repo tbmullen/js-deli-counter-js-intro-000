eda9d678aca390a75dccdf2fec3b55897fda7bf6
Trevor Mullen <tbhackworth@gmail.com>
2018-01-18 22:37:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeli = []
 
 function takeANumber(katzDeliLine, custName) {
   katzDeliLine.push(custName);
-  return "Thank you, you are number " + katzDeliLine.length + " in line.";
+  return "Welcome, " + custName + ". You are number " + katzDeliLine.length + " in line.";
 }
